5133f727f655eeb3b26c10b1f4389182dc630e92
Tereza Vavruskova <vavruskova@alpodgroup.cz>
2025-01-18 15:09:11 +0100

    About us section update

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript pro otevírání a zavírání postranního menu
+function toggleSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("active");
+}
